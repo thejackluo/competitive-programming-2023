d7751ac8df4f267cbfa4d0182b967638354653d6
Jack Luo <jack145945@gmail.com>
2023-03-05 20:28:13 -0800

    created a new usaco template

---
+++ template-old.cpp
@@ -0,0 +1,22 @@
+#include <cstdio>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    // fast input output
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    // files
+    freopen("problemname.in", "r", stdin);
+	freopen("problemname.out", "w", stdout);
+    
+
+    // input
+    int n;
+    cin >> n;
+
+
+    // output
+    return 0;
+}
