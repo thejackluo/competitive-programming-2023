a9a4c7aec6c6aee240a2b25c0e005c7bc02e1a2d
Jack Luo <jack145945@gmail.com>
2023-03-05 18:40:10 -0800

    create template.cpp for future problems

---
+++ template.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    // fast input output
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    // input
+    int n
+    cin >> n;
+
+
+    // output
+
+    return 0;
+}
