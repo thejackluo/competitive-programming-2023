7a54dfa1f71fa351c8acc4a550ad26f35ec7433a
Jack Luo <jack145945@gmail.com>
2023-03-11 23:16:47 -0800

    Testing LC 300

--- leetcode/test.cpp
+++ leetcode/test.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main() {
     // input
-    vector<int> nums = {10,9,2,5,3,7,101,18};
+    vector<int> nums = {0,1,0,3,2,3};
 
     // algorithm
 
@@ -12,7 +12,7 @@ int main() {
 
     // create two pointers on opposite ends of the vector to find the longest increasing subsequence: O(N)
     int i = 0;
-    int j = n - 1;
+    int j = n-1;
 
     // move the pointer i to the right in the condition that the next number is smaller than the current number: O(N)
     while (i < n - 1) {
@@ -25,28 +25,18 @@ int main() {
 
     // move the pointer j to the left in the condition that the next number is smaller than the current number: O(N)
     while (j > 0) {
-        if (nums[j] < nums[j - 1]) {
-            j--;
+        if (nums[j - 1] > nums[j]) {
+            --j;
         } else {
             break;
         }
     }
 
-    // print to err what the subsequence is from i to j
-    for (int k = i; k < j; k++) {
-        cerr << nums[k] << " ";
-    }
+    // bullshit
+    j++;
 
-    // interate through the sequence and find the longest increasing subsequence by keeping track of whether the sequence is increasing using a temp to keep track of each number
-    int temp = nums[i];
-    int count = 1;
-
-    for (int k = i + 1; k < j; k++) {
-        if (nums[k] > temp) {
-            count++;
-            temp = nums[k];
-        }
-    }
+    // interate through the sequence and find the longest increasing subsequence using dynamic programming
+        
 
     // output
     cout << count << endl;
