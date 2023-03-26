8385539742eac591ef677ba18c5cc77b480c2374
Jack Luo <jack145945@gmail.com>
2023-03-26 16:01:29 -0700

    Update new silver problem

---
+++ usaco/sliver/div7.cpp
@@ -0,0 +1,27 @@
+#include <cstdio>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    // fast input output
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    // files
+    freopen("div7.in", "r", stdin);
+	freopen("div7.out", "w", stdout);
+    
+    // input
+    int n;
+    cin >> n;
+
+    // input n lines of numbers that could be 0 to 1,000,000
+    int a[n];
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+
+
+    // output
+    return 0;
+}
